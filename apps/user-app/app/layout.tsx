ee5e9df783d9ebf4d08f959301707804f70bcc4e
Hemant2335 <knrt73373@gmail.com>
2024-04-15 13:41:23 +0530

    Fixed_Minor_bugs

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Providers } from "../providers";
+import { Providers } from "../provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Providers>
-      <body className={inter.className}>{children}</body>
+        <body className={inter.className}>{children}</body>
       </Providers>
     </html>
   );
